8adc679711deab82fd3a235dbb224a1af3cee57e
Garrett <gflynn@usc.edu>
2020-09-29 12:26:16 -0500

    Updated tab name and project description

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -3,5 +3,24 @@
         <h1>Projects</h1>
         <h3>Livewire: A Stimulating Night of Neurotechnology</h3>
         <p>Livewire is an immersive entertainment event in March 5th, 2021.</p>
+
+        <h3>Invocations</h3>
+        <p>Invocations is Mousai's weekly newsletter.</p>
+
+        <h3>Brainstorm</h3>
+        <p>Brainstorm is a social neurotechnology platform that
+            enables you to manage your brain data on the web and
+            couple with other minds across social, cultural, and political boundaries.</p>
+
+        <h3>Hippocampal Memory Prosthesis (HMP)</h3>
+        <p>The HMP is a proof-of-concept system that
+            delivers precise patterns of electrical stimulation
+            to the brains of human epilepsy patients and
+            significantly improves memory formation.</p>
+        <p>We are collaborating with the USC Center for Neural
+            Engineering and the neuroethics thrust at the UW Center
+            for Neurotechnology to incorporate user feedback into the
+            future design of memory neurotechnologies—including the HMP.</p>
+
     </section>
 </template>
