ba82dae2f9cd51be413e8d7a05c50f37845290f8
Garrett <gflynn@usc.edu>
2020-10-14 07:15:09 -0500

    deploy

--- commit.sh
+++ commit.sh
@@ -8,7 +8,7 @@ set -e
 
 git init
 git add -A
-git commit -m 'major UI overhaul for Brainstorm'
+git commit -m 'major UI overhaul'
 
 # if you are deploying to https://<USERNAME>.github.io
  git push -f git@github.com:mousai-neurotechnologies/mousai-neurotechnologies.github.io.git master
